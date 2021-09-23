efc2feccb0051c076a0b9b964ce9ce4b21164d0e
Srinivasa Vasu <srinivasan.surprise@gmail.com>
2021-09-23 13:13:42 +0530

    update load generator
    Signed-off-by: Srinivasa Vasu <srinivasan.surprise@gmail.com>

--- src/main/java/org/springframework/samples/petclinic/simulator/LoadController.java
+++ src/main/java/org/springframework/samples/petclinic/simulator/LoadController.java
@@ -1,23 +1,71 @@
 package org.springframework.samples.petclinic.simulator;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/simulate")
-public class LoadGenerateController {
+public class LoadController {
+
+	private final Logger LOG = LoggerFactory.getLogger(LoadController.class);
 
 	private final LoadRepository loadRepository;
 
-	public static final int MAX_SEQ = 10000;
+	private final ExecutorService LOAD_EXECUTOR = Executors.newSingleThreadExecutor();
 
-	public LoadGenerateController(final LoadRepository repository) {
+	private Future loadRun;
+
+	public static final int MAX_SEQ = 500000;
+
+	public static final int MAX_LOOP = 5;
+
+	public LoadController(final LoadRepository repository) {
 		this.loadRepository = repository;
 	}
 
+	@GetMapping("/{seq}/{loop}")
+	public void generateLoadInLoop(@PathVariable int seq, @PathVariable int loop) {
+		for (int i = 1; i <= (loop > MAX_LOOP ? MAX_LOOP : loop); i++) {
+			loadRepository.generateLoad(seq > MAX_SEQ ? MAX_SEQ : seq);
+		}
+	}
+
+	@GetMapping("/{seq}")
 	public void generateLoad(@PathVariable int seq) {
-		loadRepository.generateLoad(seq > MAX_SEQ ? MAX_SEQ : seq);
+		if (loadRun == null) {
+			synchronized (this) {
+				loadRun = LOAD_EXECUTOR.submit(() -> {
+					int i = 1;
+					while (true) {
+						int count = loadRepository.generateLoad(seq > MAX_SEQ ? MAX_SEQ : seq);
+						LOG.info("Loaded " + count + " records in iteration " + i++);
+						Thread.sleep(5000);
+					}
+				});
+			}
+		}
+	}
+
+	@GetMapping("/abort")
+	public void generateLoad() {
+		if (loadRun != null) {
+			synchronized (loadRun) {
+				loadRun.cancel(true);
+				LOG.info("Load job termination status: " + loadRun.isCancelled());
+				if (loadRun.isCancelled()) {
+					loadRun = null;
+				}
+			}
+		}
 	}
 
 }
--- src/main/java/org/springframework/samples/petclinic/simulator/LoadRepository.java
+++ src/main/java/org/springframework/samples/petclinic/simulator/LoadRepository.java
@@ -1,2 +1,21 @@
-package org.springframework.samples.petclinic.simulator;public class LoadRepository {
+package org.springframework.samples.petclinic.simulator;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class LoadRepository {
+
+	@PersistenceContext
+	EntityManager entityManager;
+
+	@Transactional
+	public int generateLoad(int seq) {
+		return entityManager.createNativeQuery("INSERT INTO LOAD_GENERATE SELECT ID FROM GENERATE_SERIES(1, :seq) ID")
+				.setParameter("seq", seq).executeUpdate();
+	}
+
 }
